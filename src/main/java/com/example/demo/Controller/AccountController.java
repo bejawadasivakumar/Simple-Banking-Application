2851202de9b3c82885617bbf58052250c9b99c70
bejawadasivakumar <2020cse.r15@svce.edu.in>
2025-06-02 21:36:18 +0530

    sample statement added in the Controller

--- src/main/java/com/example/demo/Controller/AccountController.java
+++ src/main/java/com/example/demo/Controller/AccountController.java
@@ -125,6 +125,9 @@ public class AccountController {
            Fetch all transactions for a given account number, ordered by the timestamp field in 
            descending order (i.e., most recent transactions first).
 	     */
+		/*
+		 commands are added
+		 */
 
 	}
 }
