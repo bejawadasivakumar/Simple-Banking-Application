ce41a2202528339977ad0c29c2ad284ac42b1911
bejawadasivakumar <2020cse.r15@svce.edu.in>
2025-06-12 21:20:17 +0530

    changed from GET to DELETE mapping

--- src/main/java/com/example/demo/Controller/AccountController.java
+++ src/main/java/com/example/demo/Controller/AccountController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -127,7 +128,7 @@ public class AccountController {
 	     */
 		
 	}
-	@GetMapping("/deleteAccount/{accountnumber}")
+	@DeleteMapping("/deleteAccount/{accountnumber}")
 	public ResponseEntity<String> delete(@PathVariable String accountnumber){
 		AccountDetails account = accountService.getAccountDetailsByAccountnumber(accountnumber);
 		accountService.deleteAccount(accountnumber);
